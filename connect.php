c81626e4db1027d82fad25d2d046ab26cdfef6c3
root <you@example.com>
2020-12-27 16:39:10 +0100

    heroku deploy

--- connect.php
+++ connect.php
@@ -1,8 +1,9 @@
 <?php
 
-	$host="localhost";
-	$db_user="root";
-	$db_password="";
-	$db_name="NovaArt";
+	$host="ec2-54-217-206-236.eu-west-1.compute.amazonaws.com";
+	$db_user="wtdwzakjdmfbmf";
+	$db_password="dadc27dba63e0e8bb8da12deeb852f49b795ac1114d319ccc6b31c0c5148ecb2";
+	$db_name="d8d7k1ve11a4fg";
+	$port="5432";
 
 ?>
