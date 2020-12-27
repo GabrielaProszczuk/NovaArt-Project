94d09adbe988387c4cac580d4070bffd257b3f17
root <you@example.com>
2020-12-27 17:44:47 +0100

    Remote database changes

--- connect.php
+++ connect.php
@@ -1,9 +1,13 @@
 <?php
-
-	$host="ec2-54-217-206-236.eu-west-1.compute.amazonaws.com";
-	$db_user="wtdwzakjdmfbmf";
-	$db_password="dadc27dba63e0e8bb8da12deeb852f49b795ac1114d319ccc6b31c0c5148ecb2";
-	$db_name="d8d7k1ve11a4fg";
-	$port="5432";
-
+	// Develepment connection
+	//$host="localhost";
+	//$db_user="root";
+	//$db_password="";
+	//$db_name="NovaArt";
+	
+	//Remote Connection
+	$host="remotemysql.com";
+	$db_user="fTC0bx6cvc";
+	$db_password="FIeXMzQtQN";
+	$db_name="fTC0bx6cvc";
 ?>
