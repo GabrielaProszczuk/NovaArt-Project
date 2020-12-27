0d2e52f8916cfe21c2b57234dd8e0fe80a11f3e3
root <you@example.com>
2020-12-27 15:36:36 +0100

    bootstrap changes

--- visitProfile.php
+++ visitProfile.php
@@ -250,6 +250,6 @@
 	?>
 	<script src="lightbox2-2.11.3/src/js/lightbox.js"></script>
 	 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    	<script src="../bootstrap/js/bootstrap.min.js"></script>
+    	<script src="bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>								
